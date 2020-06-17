dc77b0eeaf57b463b85fe408092cead6d23d6d6d
[name]git config --global user.name [name] <omkaraprada@mail.ru>
2020-06-17 10:18:47 +0300

    5th task of lesson 2

---
+++ lesson_2.5.py
@@ -0,0 +1,16 @@
+my_list = [9, 7, 4, 1, 1]
+print(f"Рейтинг - {my_list}")
+digit = int(input("Введите число (108 - выход) "))
+while digit != 108:
+    for el in range(len(my_list)):
+        if my_list[el] == digit:
+            my_list.insert(el + 1, digit)
+            break
+        elif my_list[0] < digit:
+            my_list.insert(0, digit)
+        elif my_list[-1] > digit:
+            my_list.append(digit)
+        elif my_list[el] > digit and my_list[el + 1] < digit:
+            my_list.insert(el + 1, digit)
+    print(f"текущий список - {my_list}")
+    digit = int(input("Введите число "))
